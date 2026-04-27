7fddbf40ce3047ae451aa83191a1601601b9a78c
cjindart <cjindart7@gmail.com>
2026-04-27 10:50:06 -0700

    finished formatting pipeline to run for new games

--- extract_frames.py
+++ extract_frames.py
@@ -3,7 +3,7 @@ Water polo frame extractor
 Takes functions from extract_labels, and parses game film to label frames
 """
 
-from extract_labels import parse_instances, get_label
+from extract_labels import *
 import subprocess
 import os
 from tqdm import tqdm
@@ -21,25 +21,31 @@ def get_video_duration(file_path) -> float:
 5. Extract that frame from the video using ffmpeg
 6. Save it into the correct folder based on the label
 """
-def main():
-    XML_PATH = "datasets/game_1_test/game_1_test.xml"
-    VIDEO_PATH = "datasets/game_1_test/game_1_test.mp4"
-
-    instances, codes = parse_instances(XML_PATH)
+def format(game_name: str, xml_path: str, video_path: str):
+    instances, codes = parse_instances(xml_path)
 
     for code in codes:
-        path = f"datasets/game_1_test/frames/{code}"
+        path = f"datasets/{game_name}/frames/{code}"
         os.makedirs(path, exist_ok=True)
 
-    duration = get_video_duration(VIDEO_PATH)
+    duration = get_video_duration(video_path)
 
-    print(f"Creating labeled frames for {VIDEO_PATH}")
+    print("="*100)
+    print(f"Creating labeled frames for {video_path}")
     for time in tqdm(range(0, int(duration), 5)):
         label = get_label(time, instances, codes)
         if label != "N/A":
-            arglist = ["ffmpeg", "-ss", str(time), "-i", VIDEO_PATH, "-frames:v", "1", f"datasets/game_1_test/frames/{label}/frame_{time:06d}s.jpg"]
+            arglist = ["ffmpeg", "-ss", str(time), "-i", video_path, "-frames:v", "1", f"datasets/{game_name}/frames/{label}/frame_{time:06d}s.jpg"]
             subprocess.run(arglist, capture_output=True, text=True)
     print("Done!")
+    print("="*100)
+
+
+def main():
+    dir = input("Paste name of game (game folder needs an XML, MP4 in it): ")  # game_1_test
+    xml = f"datasets/{dir}/{dir}.xml"
+    video = f"datasets/{dir}/{dir}.mp4"
+    format(str(dir), str(xml), str(video))
 
 
 if __name__ == "__main__":
